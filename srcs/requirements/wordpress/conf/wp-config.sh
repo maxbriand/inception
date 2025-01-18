cccae0e82695d9da8fe1813f857e9ccb029ec94a
mbriand <mbriand@inception.myguest.virtualbox.org>
2025-01-18 01:37:41 +0100

    Full inception configuration done

--- srcs/requirements/wordpress/conf/wp-config.sh
+++ srcs/requirements/wordpress/conf/wp-config.sh
@@ -1,16 +1,30 @@
-sleep 10 
+#!/bin/sh
 
-cd /var/www/wordpress
+sleep 10
 
-# if [ ! -f wp-config.php ]; then
-wp core download --allow-root
-wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'
-wp core install --allow-root --url="mbriand.42.fr" --title="Hello" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --path='/var/www/wordpress'
-wp user create --allow-root $WP_LOGIN $WP_EMAIL --user_pass=$WP_USER_PWD --role=subscriber
-chmod -R 775 wp-content
-echo "wp-config.php done"
-# else
-#   echo "wp-config.php already exists."
-# fi
+cd /var/www/html
+
+if [ ! -f wp-config.php ]; then
+    echo "wp-config.php not found"
+    wp.phar core download --allow-root
+    wp.phar config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/html'
+    wp.phar core install --allow-root --url="mbriand.42.fr" --title="Hello" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --skip-email --path='/var/www/html'
+    wp.phar user create --allow-root $WP_USER $WP_EMAIL --user_pass=$WP_USER_PWD --role=author
+
+    wp.phar config set WP_DEBUG true --allow-root
+    wp.phar config set WP_DEBUG_log true --allow-root
+    wp.phar config set WP_DEBUG_DISPLAY false --allow-root
+
+    sudo chown -R "www-data:www-data" "/var/www/html"
+    sudo find /var/www/html -type d -exec chmod 777 {} \;
+    sudo find /var/www/html -type f -exec chmod 777 {} \;
+    sudo chmod -R 777 /var/www/html/wp-content
+
+    echo "wp-config.php done"
+else
+    echo "wp-config.php found"
+fi
+
+cd /
 
 php-fpm7.4 -F
